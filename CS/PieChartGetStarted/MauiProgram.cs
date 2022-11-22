2af2d0b83c2778752f4e405ef00b680e927fc6fb
sameishchev.anton <anton.sameishchev@devexpress.com>
2022-11-22 12:44:22 +0400

    PieChart Example added

---
+++ CS/PieChartGetStarted/MauiProgram.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Hosting;
+using DevExpress.Maui.Charts;
+using PieChartGetStarted;
+using DevExpress.Maui;
+
+namespace PieChartGetStarted {
+    public static class MauiProgram {
+        public static MauiApp CreateMauiApp() {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseDevExpress()
+                .ConfigureFonts(fonts => {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+            return builder.Build();
+        }
+    }
+}
